58b75c2e4265dc2e84de0ccafaf3a3a7633fb58a
unknown <alfredronning@gmail.com>
2017-10-24 20:21:51 +0200

    funksjoner i account

--- task1-project/src/main/scala/Account.scala
+++ task1-project/src/main/scala/Account.scala
@@ -1,5 +1,11 @@
 class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
-  def withdraw(amount: Double): Unit = ??? // Implement
-  def deposit(amount: Double): Unit = ??? // Implement
-  def getBalanceAmount: Double = ??? // Implement
+  def withdraw(amount: Double): Unit = {
+    initialBalance-=amount;
+  }
+  def deposit(amount: Double): Unit = {
+    initialBalance+=amount;
+  }
+  def getBalanceAmount: Double = {
+    return initialBalance;
+  }
 }
