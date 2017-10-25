ebec84fb3fc2918a5d0472c4aee661b8f7c9c059
Ine Lofthus <inelofthus@gmail.com>
2017-10-25 11:45:17 +0200

    Task 1 9/10

--- task1-project/src/main/scala/Account.scala
+++ task1-project/src/main/scala/Account.scala
@@ -1,5 +1,21 @@
-class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
-  def withdraw(amount: Double): Unit = ??? // Implement
-  def deposit(amount: Double): Unit = ??? // Implement
-  def getBalanceAmount: Double = ??? // Implement
+import exceptions.NoSufficientFundsException, exceptions.IllegalAmountException
+
+class Account(var initialBalance: Double, val uid: Int = Bank getUniqueId) {
+  def withdraw(amount: Double): Unit = {
+    if(amount>initialBalance){
+      throw new NoSufficientFundsException;
+    } else if (amount < 0) {
+      throw new IllegalAmountException;
+    } else {
+      initialBalance -= amount
+    }
+  } // Implement
+  def deposit(amount: Double): Unit = {
+    if (amount >= 0) {
+      initialBalance += amount;
+    } else {
+      throw new IllegalAmountException;
+    }
+  } // Implement
+  def getBalanceAmount: Double = initialBalance // Implement
 }
--- task1-project/src/main/scala/Bank.scala
+++ task1-project/src/main/scala/Bank.scala
@@ -1,12 +1,25 @@
+import exceptions.NoSufficientFundsException, exceptions.IllegalAmountException
+
 object Bank {
 
   private var idCounter: Int = 0
 
-  def transaction(from: Account, to: Account, amount: Double): Unit = ??? // Implement
+  def transaction(from: Account, to: Account, amount: Double): Unit = {
+    if (amount >= 0) {
+      try {
+        from.withdraw(amount);
+        to.deposit(amount);
+      } catch{
+        case nsfe: NoSufficientFundsException => throw new NoSufficientFundsException;
+      }
+    } else {
+      throw new IllegalAmountException;
+    }
+  } // Implement
 
   def getUniqueId: Int = {
     idCounter += 1 // Can this be improved?
     idCounter
   }
-
 }
+
